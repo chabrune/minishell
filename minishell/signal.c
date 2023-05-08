40c8514292751eab211c1719386a9e2bc2d51177
Emma Muller <emuller@k0r2p4.42mulhouse.fr>
2023-05-08 17:09:01 +0200

    on craint

---
+++ minishell/signal.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signal.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emuller <emuller@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 13:38:43 by emuller           #+#    #+#             */
+/*   Updated: 2023/05/08 17:04:36 by emuller          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	control_c(int sig)
+{
+	(void)sig;
+	stop_heredoc = 1;
+	error_num = 1;
+	if (in_heredoc == 0)
+	{
+		printf("\n");
+		rl_redisplay();
+		printf("coucou");
+	}
+	if (in_cmd)
+	{
+		if (in_heredoc == 1)
+		{
+			rl_replace_line("", 0);
+			rl_on_new_line();
+			exit(0);
+		}
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		error_num = 130;
+		return ;
+	}
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+}
+
+void	handle_signal(void)
+{
+	signal(SIGINT, control_c);
+	signal(SIGQUIT, SIG_IGN);
+	rl_catch_signals = 0;
+}
