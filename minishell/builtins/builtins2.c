c49137bef5e1cf987128251477c45b9e5f31fa80
Emma Muller <emuller@k0r3p4.42mulhouse.fr>
2023-05-13 16:29:37 +0200

    aaaaaaaah

--- minishell/builtins/builtins2.c
+++ minishell/builtins/builtins2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chabrune <charlesbrunet51220@gmail.com>    +#+  +:+       +#+        */
+/*   By: emuller <emuller@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 18:22:03 by chabrune          #+#    #+#             */
-/*   Updated: 2023/05/12 22:32:02 by chabrune         ###   ########.fr       */
+/*   Updated: 2023/05/13 14:42:11 by emuller          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,17 +32,17 @@ void	choose_builtins_one_split(t_simple_cmds *cmds, t_tools *tools)
 	if (ft_strncmp("pwd", cmds->str[0], 3) == 0)
 	{
 		my_pwd(tools);
-		exit(1);
+		exit(0);
 	}
 	else if (ft_strncmp("echo", cmds->str[0], 4) == 0)
 	{
 		my_echo(cmds);
-		exit(1);
+		exit(0);
 	}
 	else if (ft_strncmp("env", cmds->str[0], 3) == 0)
 	{
 		my_env(tools, cmds);
-		exit(1);
+		exit(0);
 	}
 }
 
@@ -58,7 +58,7 @@ void	choose_builtins_one_split_two(t_simple_cmds *cmds, t_tools *tools,
 	else if (ft_strncmp("export", cmds->str[0], 6) == 0 && !cmds->str[1])
 	{
 		my_export(tools, cmds, -1, 0);
-		exit(1);
+		exit(0);
 	}
 	else if (ft_strncmp("cd", cmds->str[0], 2) == 0)
 		my_cd(tools, cmds);
