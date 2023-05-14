005b030391a1e2442c0353bfc231e67c62ce97b7
Charles Brunet <chabrune@k0r3p3.42mulhouse.fr>
2023-05-14 16:00:30 +0200

    ok

--- minishell/builtins/my_cd2.c
+++ minishell/builtins/my_cd2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   my_cd2.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emuller <emuller@student.42.fr>            +#+  +:+       +#+        */
+/*   By: chabrune <charlesbrunet51220@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 21:29:26 by chabrune          #+#    #+#             */
-/*   Updated: 2023/05/14 15:07:36 by emuller          ###   ########.fr       */
+/*   Updated: 2023/05/14 15:58:46 by chabrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,17 @@ void	my_cd(t_tools *tools, t_simple_cmds *cmds)
 	char	*old_pwd;
 	int		check_err;
 
+	home = NULL;
+	pwd = NULL;
 	find_pwd(tools, &home, &pwd);
 	if (!home)
+	{
+		if (pwd)
+			free(pwd);
 		ft_putendl_fd("minishell: cd: HOME not set", 2);
+		g_global.error_num = 1;
+		return ;
+	}
 	old_pwd = ft_strdup(pwd);
 	if (!(cmds->str[1]))
 		check_err = change_dir(tools, home, old_pwd);
